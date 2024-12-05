89241b0201844aed01f5b3c92e77c36a5fb64903
Sandro Sikic <sandro.sikic@gmail.com>
2024-12-05 11:17:48 +0100

    update

--- configs/setup/debian.sh
+++ configs/setup/debian.sh
@@ -13,7 +13,7 @@ apt update
 
 apt install docker-ce -y
 
-curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 

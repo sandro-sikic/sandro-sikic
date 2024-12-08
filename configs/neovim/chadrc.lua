d707273d95264bd1c65d6e0f0c08c927b97e95e2
Sandro Sikic <sandro.sikic@gmail.com>
2024-12-08 06:23:26 +0100

    update

--- configs/neovim/chadrc.lua
+++ configs/neovim/chadrc.lua
@@ -1,19 +1,7 @@
 local M = {}
 
--- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
-
-M.ui = {
+M.base64 = {
   theme = "catppuccin",
-  theme_toggle = { "catppuccin", "one_light" },
-
-  hl_override = highlights.override,
-  hl_add = highlights.add,
 }
-
-M.plugins = "custom.plugins"
-
--- check core.mappings for table structure
-M.mappings = require "custom.mappings"
 
 return M

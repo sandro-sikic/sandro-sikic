08c315a88fe6f8531e7565de21f1c238fcb8f04f
Sandro Sikic <sandro.sikic@gmail.com>
2024-12-08 19:45:56 +0100

    update

--- configs/neovim/mappings.lua
+++ configs/neovim/mappings.lua
@@ -4,3 +4,10 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>", { desc = "CMD enter normal mode" })
+
+
+local ooo = function(client, bufnr)
+  nvlsp.on_attach(client, bufnr)
+  -- map HERE
+  vim.keymap.set("n", "gd", "<cmd> Telescope<cr>", { buffer = bufnr })
+end

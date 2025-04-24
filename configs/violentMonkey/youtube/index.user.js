1ce4aa68918927f04c582cf9e3ee8974ff53c169
Sandro Sikic <sandro.sikic@gmail.com>
2025-04-24 18:39:24 +0200

    update

--- configs/violentMonkey/youtube/index.user.js
+++ configs/violentMonkey/youtube/index.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Redirect to tsyoutube by clicking on the video title
-// @version     1.0
+// @version     1.1.0
 // @match       https://www.youtube.com/watch*
 // @grant       none
 // @icon        https://www.gstatic.com/youtube/img/branding/favicon/favicon_144x144_v2.png
@@ -8,14 +8,17 @@
 // ==/UserScript==
 
 function main() {
+	const id = 'tsyoutube';
 	const observer = new MutationObserver(() => {
 		if (document.getElementById('tsyoutube')) return;
-		const title = document.getElementById('title').querySelector('h1');
 
+		const title = document.getElementById('title')?.querySelector('h1');
 		if (!title) return;
+		if (title.hasAttribute('data-listeners-added')) return;
 
 		title.style.cursor = 'pointer';
 		title.id = 'tsyoutube';
+
 		title.addEventListener('click', (e) => {
 			window.location =
 				'https://www.tsyoutube.com' +
@@ -28,6 +31,7 @@ function main() {
 		title.addEventListener('mouseout', () => {
 			title.style.textDecoration = 'none';
 		});
+		title.setAttribute('data-listeners-added', 'true');
 	});
 
 	observer.observe(document.body, { childList: true, subtree: true });
